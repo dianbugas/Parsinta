f572c94204ace98761a63b170602f7b918d458d7
dianbugas <dianbugas@gmail.com>
2021-12-21 22:51:35 +0700

    ok

--- next-tailwind/pages/index.js
+++ next-tailwind/pages/index.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='bg-rose-500 min-h-screen'>
-
+    <div className="max-w-xl">
+      <div className="overflow-hidden bg-white border rounded-lg shadow-sm shadow">
+        <header className="border-b px-4 py-2.5 bg-gray-50/50 flex items-center justify-between">
+          <div>
+            <h1 className="font-medium">
+              Card title
+            </h1>
+            <span className="text-gray-500">Lorem ipsum dolor sit amet consectetur.</span>
+          </div>
+          <a href="#" className="bg-pink-500 hover:bg-pink-700 transition duration-300 focus:outline-none focus:ring focus:ring-pink-200 text-white px-5 py-2.5 rounded-lg font-semibold text-sm">News Task</a>
+        </header>
+        <section className="px-4 py-2.5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, assumenda? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, repudiandae accusantium id soluta nulla veniam molestias. Nemo molestiae illo sunt.
+        </section>
+        <footer className="px-4 py-2.5 border-t text-gray-500 bg-gray-50/50">Lorem ipsum dolor sit amet consectetur adipisicing elit.</footer>
+      </div>
     </div>
   )
 }
