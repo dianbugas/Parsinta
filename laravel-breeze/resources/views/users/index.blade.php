b074494d128a7b7fb624529d99cdab267c8b88de
dianbugas <dianbugas@gmail.com>
2023-04-25 11:11:42 +0700

    laravel breeze

--- laravel-breeze/resources/views/users/index.blade.php
+++ laravel-breeze/resources/views/users/index.blade.php
@@ -10,6 +10,10 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged User page!") }}
+                    <p>
+                        <a href="{{ route('users.create') }}" class="text-blue-500 hover:text-blue-600 underline">Create
+                            new User</a>
+                    </p>
                 </div>
             </div>
         </div>
